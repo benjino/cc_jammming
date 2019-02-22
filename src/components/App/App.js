f115162c517fc4d104779602fdac61b6de1348d7
Ben Siegfried <siegfriedmedia@gmail.com>
2019-02-22 10:33:09 -0800

    create a method that saves the playlist to a user's account

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -25,6 +25,7 @@ class App extends React.Component {
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
   }
 
   addTrack(track) {
@@ -39,6 +40,16 @@ class App extends React.Component {
 
   updatePlaylistName(name){
     this.setState({playlistName: name});
+  }
+
+  savePlaylist(){
+    let trackURIs = this.state.playlistTracks.map(track => track.uri);
+    Spotify.savePlaylist(this.state.playlistName, trackURIs).then(() => {
+        this.setState({
+          playlistName: 'New Playlist',
+          playlistTracks: []
+        });
+      });
   }
 
   render() {
@@ -56,7 +67,8 @@ class App extends React.Component {
               playlistName={this.state.playlistName}
               playlistTracks={this.state.playlistTracks}
               onRemove={this.removeTrack}
-              onNameChange={this.updatePlaylistName} />
+              onNameChange={this.updatePlaylistName}
+              onSave={this.savePlaylist} />
           </div>
         </div>
       </div>
